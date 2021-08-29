86e18f5bfb2951f1dd0131fe6fc8b61f16fa6522
Amin_Seifi <aminseiifi@gmail.com>
2021-08-29 18:05:53 +0430

    Connect CCI Strategy class to Telegram Bot

--- Strategy/CCI.py
+++ Strategy/CCI.py
@@ -1,15 +1,17 @@
 from CoreData.IEXCloud import IEXCloud
 from CoreData.TAAPI import TAAPI
+from Notification.Telegram import Telegram
 import time
 
 
 class CCI:
 
-    def __init__(self, IEXCloud_obj: IEXCloud, TAAPI_obj: TAAPI):
+    def __init__(self, IEXCloud_obj: IEXCloud, TAAPI_obj: TAAPI, Telegram_obj: Telegram):
         self.__iex = IEXCloud_obj
         self.__taapi = TAAPI_obj
+        self.__tel = Telegram_obj
 
-    def cci_5m(self, symbol_iex, symbol_ta):
+    def cci_5m(self, symbol_iex, symbol_ta, verbose=True):
         goal_coefficient = 1.00350
         stop_coefficient = 0.99280
         current_price = None
@@ -20,7 +22,8 @@ class CCI:
                                          interval='5m')
 
         if -100 < cci_value:
-            print(f'[-] Not found good entry point (cci = {cci_value})')
+            msg = f'[-] Not found good entry point (cci = {cci_value})'
+            print(msg)
         else:
             while cci_value < -100:
                 cci_value = self.__taapi.get_cci(symbol=symbol_ta,
@@ -29,10 +32,14 @@ class CCI:
             current_price = float(self.__iex.get_quote(symbol_iex)['latestPrice'])
             goal_price = goal_coefficient * current_price
             stop_price = stop_coefficient * current_price
-            print(f'[+] Order executed!\n'
-                  f'\tCurrent_price = {current_price}$\n'
-                  f'\tTarget_price = {goal_price}$ ({(goal_coefficient - 1) * 100}%)\n'
-                  f'\tStopLoss_price = {stop_price}$ ({(stop_coefficient - 1) * 100}%).')
+            msg = f'[+] Order executed!\n' \
+                  f'\tCurrent_price = {current_price}$\n' \
+                  f'\tTarget_price = {goal_price}$ ({(goal_coefficient - 1) * 100}%)\n' \
+                  f'\tStopLoss_price = {stop_price}$ ({(stop_coefficient - 1) * 100}%).'
+            if verbose:
+                self.__tel.msg_channel(msg)
+
+            print(msg)
             while stop_price < current_price < goal_price:
                 status = True
                 current_price = float(self.__iex.get_quote(symbol_iex)['latestPrice'])
@@ -41,6 +48,12 @@ class CCI:
                 time.sleep(15)
             status = False
             if current_price >= goal_price:
-                print(f'[+] You earn {(goal_coefficient - 1) * 100}% of your account! nice job.')
+                msg = f'[+] You earn {(goal_coefficient - 1) * 100}% of your account! nice job.'
+                if verbose:
+                    self.__tel.msg_channel(msg)
+                print(msg)
             elif current_price <= stop_price:
-                print(f'[-] You loss {(stop_coefficient - 1) * 100}% of your account! see you soon.')
+                msg = f'[-] You loss {(stop_coefficient - 1) * 100}% of your account! see you soon.'
+                if verbose:
+                    self.__tel.msg_channel(msg)
+                print(msg)
--- main.py
+++ main.py
@@ -11,11 +11,16 @@ from secrets import TELEGRAM_BOT_API_TOKEN
 try:
     iex_obj = IEXCloud(IEX_CLOUD_API_TOKEN)
     taapi_obj = TAAPI(TAAPI_API_TOKEN)
+    telegram_obj = Telegram(token=TELEGRAM_BOT_API_TOKEN,
+                            channel_id=TELEGRAM_CHANNEL_ID)
     cci_obj = CCI(IEXCloud_obj=iex_obj,
-                  TAAPI_obj=taapi_obj)
+                  TAAPI_obj=taapi_obj,
+                  Telegram_obj=telegram_obj)
+    telegram_obj.msg_channel('[+] Start working!')
     while True:
         cci_obj.cci_5m(symbol_iex='BTCUSDT',
-                       symbol_ta='BTC/USDT')
+                       symbol_ta='BTC/USDT',
+                       verbose=True)
         time.sleep(15)
 
 except Exception as ex:
