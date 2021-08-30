7ca5a329b72e928f2b9b1d259260ccb8df86ba8a
Amin_Seifi <aminseiifi@gmail.com>
2021-08-30 17:48:42 +0430

    Append some comments to main and IEXCloud and Create get_price method in IEXCloud

--- CoreData/IEXCloud.py
+++ CoreData/IEXCloud.py
@@ -1,18 +1,75 @@
 import pandas as pd
 import requests
 
+# What is IEX Cloud?
+"""
+    IEX Cloud is a platform that makes financial data and services accessible to everyone.
+    IEXCloud Main URL: https://iexcloud.io/
+    IEXCloud API Documentation URL: https://iexcloud.io/docs/api
+"""
+
 
 class IEXCloud:
 
-    def __init__(self, token):
+    def __init__(self, token):  # A simple constructor from IEXCloud class
         self.__BASE_URL = 'https://cloud.iexapis.com/stable/crypto'
         self.__API_TOKEN = token
 
-    def get_quote(self, symbol):
-        url = f'{self.__BASE_URL}/{symbol}/quote?token={self.__API_TOKEN}'
-        columns = ['symbol', 'price', 'low', 'high']
-        result = requests.get(url)
-        if result.status_code != 200:
-            raise RuntimeError('IEXCloud: cannot connect to url (err code: %s)!' % result.status_code)
+    def get_quote(self, symbol):  # This method return quote(info) of your crypto that identified by a symbol
+        url = f'{self.__BASE_URL}/{symbol}/quote?token={self.__API_TOKEN}'  # Create API URL
+        result = requests.get(url)  # Send GET request to IEX Cloud servers
+        if result.status_code != 200:  # If the result of your request is not ok throw exception
+            raise RuntimeError('IEXCloud (get_quote): cannot connect to url (err code: %s)!' % result.status_code)
 
-        return result.json()
+        return result.json()  # return the json parsed result
+
+    # Using this method for get price is more efficiency than get quote method
+    def get_price(self, symbol):  # This method return price of your crypto that identified by a symbol
+        url = f'{self.__BASE_URL}/{symbol}/price?token={self.__API_TOKEN}'  # Create API URL
+        result = requests.get(url)  # Send GET request to IEX Cloud servers
+        if result.status_code != 200:  # If the result of your request is not ok throw exception
+            raise RuntimeError('IEXCloud (get_price): cannot connect to url (err code: %s)!' % result.status_code)
+
+        return float(result.json()['price'])  # return the result as a floating point number
+
+
+# Error Codes
+"""
+    IEX Cloud uses HTTP response codes to indicate the success or failure of an API request.
+
+    General HTML status codes
+    
+    2xx Success.
+    
+    4xx Errors based on information provided in the request
+    
+    5xx Errors on IEX Cloud servers
+    
+    IEX Cloud HTTP Status Codes
+    HTTP CODE	TYPE	DESCRIPTION
+    400	Incorrect Values	Invalid values were supplied for the API request
+    400	No Symbol	No symbol provided
+    400	Type Required	Batch request types parameter requires a valid value
+    401	Authorization Restricted	Hashed token authorization is restricted
+    401	Authorization Required	Hashed token authorization is required
+    401	Restricted	The requested data is marked restricted and the account does not have access.
+    401	No Key	An API key is required to access the requested endpoint.
+    401	Secret Key Required	The secret key is required to access to requested endpoint.
+    401	Denied Referer	The referer in the request header is not allowed due to API token domain restrictions.
+    402	Over Limit	You have exceeded your allotted credit quota (and pay-as-you-go is not enabled on legacy plans).
+    402	Free Tier Not Allowed	The requested endpoint is not available to free accounts.
+    402	Tier Not Allowed	The requested data is not available to your current tier.
+    403	Authorization Invalid	Hashed token authorization is invalid.
+    403	Disabled Key	The provided API token has been disabled
+    403	Invalid Key	The provided API token is not valid.
+    403	Test Token in Production	A test token was used for a production endpoint.
+    403	Production Token in Sandbox	A production token was used for a sandbox endpoint.
+    403	Circuit Breaker	Your pay-as-you-go circuit breaker has been engaged and further requests are not allowed.
+    403	Inactive	Your account is currently inactive.
+    404	Unknown Symbol	Unknown symbol provided
+    404	Not Found	Resource not found
+    413	Max Types	Maximum number of types values provided in a batch request.
+    429	Too Many Requests	Too many requests hit the API too quickly. An exponential backoff of your requests is recommended.
+    451	Enterprise Permission Required	The requested data requires additional permission to access.
+    500	System Error	Something went wrong on an IEX Cloud server.
+"""
--- main.py
+++ main.py
@@ -9,19 +9,26 @@ from secrets import TELEGRAM_CHANNEL_ID
 from secrets import TELEGRAM_BOT_API_TOKEN
 
 try:
-    iex_obj = IEXCloud(IEX_CLOUD_API_TOKEN)
-    taapi_obj = TAAPI(TAAPI_API_TOKEN)
+    iex_obj = IEXCloud(IEX_CLOUD_API_TOKEN)  # Create IEXCloud object that gives the bot last crypto price
+    taapi_obj = TAAPI(TAAPI_API_TOKEN)  # Create TAAPI object that gives the bot indicators values
     telegram_obj = Telegram(token=TELEGRAM_BOT_API_TOKEN,
-                            channel_id=TELEGRAM_CHANNEL_ID)
+                            channel_id=TELEGRAM_CHANNEL_ID)  # Create Telegram object that gives the bot the ability
+    # of sending logs to telegram channel
     cci_obj = CCI(IEXCloud_obj=iex_obj,
                   TAAPI_obj=taapi_obj,
-                  Telegram_obj=telegram_obj)
-    telegram_obj.msg_channel('[+] Start working!')
+                  Telegram_obj=telegram_obj)  # Create CCI object that gives the bot the ability of technical
+    # analysis and get along with cci indicator
+    telegram_obj.msg_channel('[+] Start working!')  # Send message as notice the user that I'm start
     while True:
-        cci_obj.cci_5m(symbol_iex='BTCUSDT',
-                       symbol_ta='BTC/USDT',
-                       verbose=True)
-        time.sleep(17)
+        try:
+            cci_obj.cci_5m(symbol_iex='BTCUSDT',
+                           symbol_ta='BTC/USDT',
+                           verbose=True)  # Call 5m cci method that signal user when put an order or when not (5 min)
+            time.sleep(17)  # Sleep for 17 second for 2 reason
+            # First. We use free version of TAAPI and for each api request we should wait for 15 sec
+            # Second. For Prevent more than 50% of the cci value noises and wait for gain more confident
+        except Exception as ex:
+            print('main: %s' % ex)
 
 except Exception as ex:
     print('main: %s' % ex)
