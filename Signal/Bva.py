0ef17c9c5a10e1ec6e2fa859628aa7ac73c61163
Amin_Seifi <aminseiifi@gmail.com>
2021-11-08 08:56:59 +0330

    Debug file Assets direction Print comments in each step Recursive method when an error occurs

--- Signal/Bva.py
+++ Signal/Bva.py
@@ -9,46 +9,61 @@ class Bva:
     def __init__(self, strategy_number: int):
         self.__NUMBER = strategy_number
         self.__BASE_URL = 'https://bitcoinvsalts.com/strat/%i' % self.__NUMBER
-        if not os.path.isfile('../Assets/%i' % self.__NUMBER):
-            f = open(f'{self.__NUMBER}.dat', 'w')
+        if not os.path.isfile('Assets/%i.dat' % self.__NUMBER):
+            f = open(f'Assets/{self.__NUMBER}.dat', 'w')
             f.write('0')
             f.close()
 
     def get_file(self, mode: str):
-        return open(f'{self.__NUMBER}.dat', mode)
+        return open(f'Assets/{self.__NUMBER}.dat', mode)
 
     def render(self):
-        session = HTMLSession()
-        r = session.get(self.__BASE_URL)
-        r.html.render(sleep=2, timeout=20)
+        print("[+] Try to connect BVA!")
+        try:
+            session = HTMLSession()
+            r = session.get(self.__BASE_URL)
+            r.html.render(sleep=2, timeout=20)
+            print("[+] Success!")
+        except Exception as ex:
+            print(ex)
+            return self.render()
         return r.html
 
     def get_trade_count(self):
-        r = self.render()
-        clean_page = BeautifulSoup(r.text, 'html.parser')
-        new_num = int(re.findall(r'Trades Count\n(\d+)', clean_page.text)[0])
+        try:
+            r = self.render()
+            clean_page = BeautifulSoup(r.text, 'html.parser')
+            new_num = int(re.findall(r'Trades Count\n(\d+)', clean_page.text)[0])
 
-        f = self.get_file('r')
-        old_num = int(f.read(5))
-        f.close()
+            f = self.get_file('r')
+            old_num = int(f.read(5))
+            f.close()
 
-        f = self.get_file('w')
-        f.write(str(new_num))
-        f.close()
+            f = self.get_file('w')
+            f.write(str(new_num))
+            f.close()
+        except Exception as ex:
+            print(ex)
+            return self.get_trade_count()
 
-        if new_num > old_num:
-            return new_num, True
-        return new_num, False
+        if new_num > old_num > 0:
+            return new_num, old_num, True
+        return new_num, old_num, False
 
     def in_position(self):
-        r = self.render()
-        clean_page = BeautifulSoup(r.text, 'html.parser')
-        last = re.findall(r'LONG\n((.+)\n(.+))', clean_page.text)[0]
-
-        while last[1] == '---':
+        try:
+            print("[+] In Position")
             r = self.render()
             clean_page = BeautifulSoup(r.text, 'html.parser')
             last = re.findall(r'LONG\n((.+)\n(.+))', clean_page.text)[0]
-            time.sleep(200)
 
-        return float(last[0]), float(last[1])
+            while last[2] == '---':
+                r = self.render()
+                clean_page = BeautifulSoup(r.text, 'html.parser')
+                last = re.findall(r'LONG\n((.+)\n(.+))', clean_page.text)[0]
+                time.sleep(10)
+
+            return float(last[1]), float(last[2])
+        except Exception as ex:
+            print(ex)
+            return self.in_position()
